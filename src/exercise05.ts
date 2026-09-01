fc1683f505ad9405e51ecd0b9b94e26161960b7a
Keith Ard <Keith.ard@outlook.com>
2026-09-01 19:21:38 -0400

    finished exercise 5

--- src/exercise05.ts
+++ src/exercise05.ts
@@ -1,9 +1,31 @@
-export type NetworkConfig = {};
+export type NetworkConfig = {
+  serverUrl: string;
+  port: number;
+};
 
-export type EnvironmentConfig = {};
+export type EnvironmentConfig = {
+  environment: 'dev' | 'prod';
+  timeout: number;
+};
 
 export type AppConfig = NetworkConfig & EnvironmentConfig;
 
 export function initializeConfig(userOverrides: Partial<AppConfig>): AppConfig {
-  return {};
+  // defaults for missing fields
+  const defaults: AppConfig = {
+    serverUrl: 'http://localhost',
+    port: 8080,
+    environment: 'dev',
+    timeout: 3000,
+  };
+
+  // created merged AppConfig object, use defaults for missing fields
+  const merged: AppConfig = {
+    serverUrl: userOverrides.serverUrl ?? defaults.serverUrl,
+    port: userOverrides.port ?? defaults.port,
+    environment: userOverrides.environment ?? defaults.environment,
+    timeout: userOverrides.timeout ?? defaults.timeout,
+  };
+
+  return merged;
 }
