62b3ea99cf0c7d47549f90bc3569531fc8d63216
Keith Ard <Keith.ard@outlook.com>
2026-09-01 19:05:36 -0400

    finished exercise 4

--- src/exercise04.ts
+++ src/exercise04.ts
@@ -1,11 +1,35 @@
-export type Circle = {};
+export type Circle = {
+  kind: 'circle';
+  radius: number;
+};
 
-export type Rectangle = {};
+export type Rectangle = {
+  kind: 'rectangle';
+  width: number;
+  height: number;
+};
 
-export type Square = {};
+export type Square = {
+  kind: 'square';
+  sideLength: number;
+};
 
 export type Shape = Circle | Rectangle | Square;
 
 export function calculateArea(shape: Shape): number {
-  return 0;
+  let area: number = 0;
+
+  switch (shape.kind) {
+    case 'circle':
+      area = Math.PI * shape.radius ** 2;
+      break;
+    case 'rectangle':
+      area = shape.width * shape.height;
+      break;
+    case 'square':
+      area = shape.sideLength ** 2;
+      break;
+  }
+
+  return area;
 }
