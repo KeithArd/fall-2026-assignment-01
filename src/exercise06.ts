2ae6a6cf130d864a82b5583beab50e0b9a982601
Keith Ard <Keith.ard@outlook.com>
2026-09-02 16:58:53 -0400

    finished exercise 6

--- src/exercise06.ts
+++ src/exercise06.ts
@@ -1,15 +1,24 @@
 export class Stack<T> {
-  public push(item: T): void {}
+  private items: T[] = []; // internal array to implement stack. to hold items of generic type T
 
+  // add item to end of array
+  public push(item: T): void {
+    this.items.push(item);
+  }
+
+  // return item from end of array
   public pop(): T | undefined {
-    return undefined;
+    return this.items.pop();
   }
 
+  // return last item in array without removing(popping) it
   public peek(): T | undefined {
-    return undefined;
+    const lastIndex = this.items.length - 1; // get last index of array
+    return this.items[lastIndex];
   }
 
+  // return the size of the array
   public size(): number {
-    return 0;
+    return this.items.length;
   }
 }
