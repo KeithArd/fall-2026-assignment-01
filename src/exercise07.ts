154426db4f38a77272c3c5a7b403580080a7753d
Keith Ard <Keith.ard@outlook.com>
2026-09-02 18:09:56 -0400

    finished exercises 7 and 9

--- src/exercise07.ts
+++ src/exercise07.ts
@@ -1,3 +1,35 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+export type Gradebook = {
+  [studentName: string]: {
+    [subject: string]: number;
+  };
+};
+
+const filePath = path.join(__dirname, '../data/gradebook.json');
+
 export function calculateSubjectAverage(subject: string): number {
-  return 0;
+  const filePath = path.join(__dirname, '../data/gradebook.json'); // build path relative to this file's location
+  const inputData = fs.readFileSync(filePath, 'utf-8'); // read in the json file
+  const gradebook: Gradebook = JSON.parse(inputData); // parse data into object with type Gradebook
+
+  let sum: number = 0; // number to hold sum of scores in given subject
+  let numOfScores: number = 0; // total number of scores to be used to calculate average
+
+  // loop through students in gradebook
+  for (const studentName in gradebook) {
+    const studentGrades = gradebook[studentName]; // get the student grades (nested object)
+
+    // if student has a grade in the given subject, then:
+    // iterate number of scores,
+    // add score to sum
+    if (subject in studentGrades) {
+      numOfScores++;
+      sum += studentGrades[subject];
+    }
+  }
+
+  // if number of scores is not zero, return average. otherwise return 0
+  return numOfScores ? sum / numOfScores : 0;
 }
--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,8 @@
 // Example:
 // import { formatName } from './exercise01.js';
 // console.log(formatName('John', 'Doe', 'Quincy'));
-import { getInventoryValue } from './exercise03.js';
-
-const inventory: Array<[string, number, number]> = [
-  ['Laptop', 10, 1],
-  ['Keyboard', 3, 2],
-  ['Mouse', 8, 3],
-  ['Monitor', 6, 4],
-  ['Headphones', 5, 5],
-];
-
-console.log(getInventoryValue(inventory));
+import { calculateSubjectAverage } from './exercise07.js';
+import * as fs from 'fs';
+const inputData = fs.readFileSync('data/gradebook.json', 'utf-8');
+// console.log('test!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+console.log(inputData);
